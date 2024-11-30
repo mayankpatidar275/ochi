811523dadbd830f9ea97372b3a2b756b17a6e395
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-12-01 01:03:13 +0530

    feat: Responsive Navbar
    animation for hamburger svg, mobile menu animation, fonts, underline animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "FoundersGrotesk-Semibold": ["FoundersGrotesk-Semibold", "sans-serif"],
+        icomoon: ["icomoon", "sans-serif"],
+        "NeueMontreal-Regular": ["NeueMontreal-Regular", "sans-serif"],
+      },
+      keyframes: {
+        underlineAnimation2: {
+          "0%": {
+            transform: "scaleX(1)",
+            transformOrigin: "bottom right",
+          },
+          "50%": {
+            transform: "scaleX(0)",
+            transformOrigin: "bottom right",
+          },
+          "51%": {
+            transform: "scaleX(0)",
+            transformOrigin: "bottom left",
+          },
+          "100%": {
+            transform: "scaleX(1)",
+          },
+        },
+      },
+      animation: {
+        underlineAnimation2: "underlineAnimation2 0.5s linear 1 ",
+      },
+    },
   },
   plugins: [],
 };
